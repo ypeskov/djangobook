f8f007b7afe340528a2203264c5553da444e8f7c
Yuriy Peskov <yuriy.peskov@gmail.com>
2021-03-12 19:44:01 +0200

    mmm

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1bp2fs^s23jxt#yhvg$1-)h$02^hvl#u5zvg_zx7+vd9ug2t*#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
